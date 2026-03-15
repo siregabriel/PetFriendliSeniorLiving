1a4a60954abd6dc6ef5916d9f9b16124127d4656
Gabriel Rosales <gabrielrosales@MacBook-Air-de-Gabriel-2.local>
2026-03-15 12:58:22 -0600

    Revamped design and bugs cleared

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-12-15.clover', // Usa la versión que te sugiera VS Code o la más reciente
+  apiVersion: '2026-02-25.clover',
 });
 
 export async function POST(request: Request) {
